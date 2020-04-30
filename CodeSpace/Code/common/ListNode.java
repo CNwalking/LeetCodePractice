8fe8061aa644ab13be868daffa1b48b22278e0fe
huqi <huqi@zhuojianchina.com>
2020-04-30 14:01:21 +0800

    init project

---
+++ CodeSpace/Code/common/ListNode.java
@@ -0,0 +1,12 @@
+package Code.common;
+
+/**
+ * @Author: CNwalking
+ * @DateTime: 2020/4/30 1:57 下午
+ * @Description:
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ CodeSpace/Code/twoSum.java
@@ -0,0 +1,23 @@
+package Code;
+
+import Code.common.ListNode;
+
+/**
+ * @Author: CNwalking
+ * @DateTime: 2020/4/30 1:52 下午
+ * @Description:  两个数都不以 0 开头的逆序方式存储的链表,返回一个新的链表来表示它们的和
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ */
+public class twoSum {
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
